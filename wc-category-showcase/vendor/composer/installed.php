3a24d4c6064357b2e9f3a373d89a5aff510aff87
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 06:28:08 +0000

    2.2.7

---
+++ wc-category-showcase/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pluginever/wc-category-showcase',
+        'pretty_version' => 'v2.2.7',
+        'version' => '2.2.7.0',
+        'reference' => 'c0e6d63e1c3505e9e34b5e7264778da7b34b9736',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'byteever/bytekit-plugin' => array(
+            'pretty_version' => 'v1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => '794ec9834ecac6cead6b1064a605ff930864950b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../byteever/bytekit-plugin',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pluginever/wc-category-showcase' => array(
+            'pretty_version' => 'v2.2.7',
+            'version' => '2.2.7.0',
+            'reference' => 'c0e6d63e1c3505e9e34b5e7264778da7b34b9736',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
